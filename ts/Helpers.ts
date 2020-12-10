a53a99b263695d467934e6b97cedcd06c15d03e9
vanthinhit310 <vanthinhit310@gmail.com>
2020-12-10 11:24:41 +0700

    -

--- ts/Helpers.ts
+++ ts/Helpers.ts
@@ -11,5 +11,32 @@ const Helpers = {
 
 // @ts-ignore
 $(document).ready(function () {
+    // @ts-ignore
+    let sticky = $('section.Header');
+    // @ts-ignore
+    let scroll = $(window).scrollTop();
+
+    if (scroll >= 40) {
+        sticky.addClass('fixed'); }
+    else {
+        sticky.removeClass('fixed');
+
+    }
     Helpers.gotoTop();
-})
+});
+
+
+// @ts-ignore
+$(window).scroll(function() {
+    // @ts-ignore
+    let sticky = $('section.Header');
+    // @ts-ignore
+    let scroll = $(window).scrollTop();
+
+    if (scroll >= 40) {
+        sticky.addClass('fixed'); }
+    else {
+        sticky.removeClass('fixed');
+
+    }
+});
