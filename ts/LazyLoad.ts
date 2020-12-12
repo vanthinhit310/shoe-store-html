019b4fe63461ac16de70772916256106f670dbb7
vanthinhit310 <vanthinhit310@gmail.com>
2020-12-12 19:32:56 +0700

    -

--- ts/LazyLoad.ts
+++ ts/LazyLoad.ts
@@ -2,8 +2,10 @@
 const myLazyLoad = new LazyLoad({
     elements_selector: '.lazy',
     class_loading: '.processing',
+    unobserve_entered: true,
     callback_loading: (el)=>{el.parentElement.classList.add('lazy-loading')},
-    callback_loaded: (el)=>{el.parentElement.classList.remove('lazy-loading')}
+    callback_loaded: (el)=>{el.parentElement.classList.remove('lazy-loading')},
+    callback_error: (el)=>{el.parentElement.classList.remove('lazy-loading')}
 });
 
 myLazyLoad.update();
