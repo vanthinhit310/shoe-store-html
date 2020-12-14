3c5388a9f6ac85171a7c9a49d9b6ebdd07a9960b
Thinh <vanthinhit310@gmail.com>
2020-12-14 16:17:48 +0700

    -

--- ts/Anime.ts
+++ ts/Anime.ts
@@ -1,28 +1,30 @@
 const ANM = {
     refreshIcon: function () {
         let el = document.querySelector('.hover-effect-icon');
-        el.addEventListener('mouseenter', function () {
-            // @ts-ignore
-            anime.remove(el);
-            // @ts-ignore
-            anime({
-                targets: '.refresh-icon',
-                rotate: 360,
-                duration: 1000,
-                easing: 'easeInOutSine',
-            });
-        }, false);
-        el.addEventListener('mouseleave', function () {
-            // @ts-ignore
-            anime.remove(el);
-            // @ts-ignore
-            anime({
-                targets: '.refresh-icon',
-                rotate: 0,
-                duration: 1000,
-                easing: 'easeInOutSine',
-            });
-        }, false);
+        if (el){
+            el.addEventListener('mouseenter', function () {
+                // @ts-ignore
+                anime.remove(el);
+                // @ts-ignore
+                anime({
+                    targets: '.refresh-icon',
+                    rotate: 360,
+                    duration: 1000,
+                    easing: 'easeInOutSine',
+                });
+            }, false);
+            el.addEventListener('mouseleave', function () {
+                // @ts-ignore
+                anime.remove(el);
+                // @ts-ignore
+                anime({
+                    targets: '.refresh-icon',
+                    rotate: 0,
+                    duration: 1000,
+                    easing: 'easeInOutSine',
+                });
+            }, false);
+        }
     }
 };
 
